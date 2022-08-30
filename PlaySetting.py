712bf8016c4182e65d205cce7fcc05f40f9f95eb
HoriK <horishi.root2@gmail.com>
2022-08-30 14:34:12 +0900

    implement PlaySettings class

---
+++ PlaySetting.py
@@ -0,0 +1,18 @@
+class PlaySetting:
+    play_speed : float
+    play_volume : float
+
+    def __init__(self):
+        self.play_speed = 1.0
+        self.play_volume = 1.0
+
+    def change_speed(self, speed:float) -> None:
+        self.play_speed = speed
+
+    def chagne_volume(self, volume:float) -> None:
+        self.play_volume = volume 
+     
+if __name__ == "__main__":
+    play_setting = PlaySetting()
+    print(play_setting.play_speed)
+    print(play_setting.play_volume)
