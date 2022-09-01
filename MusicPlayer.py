f87062c6629548548a5cd5f9e54ca4fac57c93df
HoriK <horishi.root2@gmail.com>
2022-09-01 19:10:27 +0900

    modified the play_title() so that sound can be played music

--- MusicPlayer.py
+++ MusicPlayer.py
@@ -11,21 +11,31 @@ class MusicPlayer:
     all_songs : AllSongsList
     play_info : Music
     play_index : int #consider it !!
+    play_setting : PlaySetting
+    system_setting : SystemSetting
 
     def __init__(self):
         self.system_setting = SystemSetting()
         self.all_songs = AllSongsList(self.system_setting.search_path)
         self.play_info = None
+        self.play_setting = PlaySetting()
         #print(self.all_songs.music_list)  
         self.skip_direction = True # for test 
 
     def play_title(self,title:str) -> None :
         for i in range(len(self.all_songs.music_list)):
             if(title ==  self.all_songs.music_list[i]):
-                self.play_info = Music(title)
+                self.play_info = Music("materials/"+title)
                 self.play_index = i 
-                print("index=" + str(i))
-                print(self.play_info.file_name)
+               
+                #for test
+                self.play_info.play(self.play_setting)  
+                counter = 0
+                while(counter < 5):
+                    time.sleep(1)
+                    counter += 1
+
+
 
     def skip_song(self,direction:bool) -> None:
 
@@ -38,6 +48,7 @@ class MusicPlayer:
             print("index=" + str(self.play_index))
             print(self.play_info.file_name)
 
+            self.play_title(self.play_info.file_name) 
         else:
             ret_val = self.all_songs.skip_song_to_previous(self.play_index)
             self.play_index = ret_val[0]
@@ -46,14 +57,17 @@ class MusicPlayer:
             print("index=" + str(self.play_index))
             print(self.play_info.file_name)
 
-    def move_5seconds(self,direction:int) -> None:
+            self.play_title(self.play_info.file_name)
+ 
+    def move_5seconds(self,direction:bool) -> None:
         pass
 
-    def volume_control(self,up_or_down:int) -> None:
+    def volume_control(self,direction:bool) -> None:
         pass
 
     def change_dir(self,new_path:str) -> None:
-        pass
+        new_dir = input()
+        system_setting.change_search_path(new_dir)
 
     def change_sort_method(self) -> None:
         pass
